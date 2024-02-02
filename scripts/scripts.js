b46030f333e856c91a6503cf1b038b90997d0f41
ChrisJacobs2 <cmjacobs@uncg.edu>
2024-02-02 16:35:20 -0500

    Moved script tag to just before body tag ends. This is because the script is loading before the rest of the site does, which means it can't see anything after where it is loaded from. Therefore, it is moved so that it can access the splash screen classes. Also, I added functionaltiy to the "no" button the splash screen.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -16,3 +16,26 @@ https://patorjk.com/software/taag/#p=display&f=Graffiti&t=Type%20Something%20
 */
 
 // The question is: "Do you have soft hands?"
+
+// Get splash buttons
+var yesButtonSplash = document.querySelector('.splash-yes');
+var noButtonSplash = document.querySelector('.splash-no');
+
+// Get splash screen Div
+var splashScreen = document.querySelector('.splash');
+
+
+// Add event listeners
+yesButtonSplash.addEventListener('click', function() {
+    console.log('"Yes" button clicked');
+    // link to some silly website for goobers
+});
+
+noButtonSplash.addEventListener('click', function() {
+    console.log('"No" button clicked');
+    splashScreen.style.display = 'none';
+});
+
+/* END SPLASH SCREEN
+###########################################################################################################
+*/
