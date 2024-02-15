d1afe7e139ee1fd70c818f25f6791f7fb1ba9b34
Will Hutcheon <willhutcheon@gmail.com>
2024-02-15 00:26:50 -0500

    worked on buttons

---
+++ scripts/cart-script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const addButtons = document.querySelectorAll('.add');
+    addButtons.forEach(button => {
+        button.addEventListener('click', function () {
+
+        });
+    });
+});
